ae38311b6a6c8cb734d5e970a31381a8e1a0e7d9
Jamie Brown <jmbrowncow@gmail.com>
2018-09-17 20:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function lowerCaseDrivers(array) {
   return array.map(name => name.toLowerCase());
 }
+
+function nameToAttributes(array) {
+  return arra.map(function(fullName){
+    const first, last = fullName.split("");
+  })
+}
